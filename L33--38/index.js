a1fa8e098d2b9b74974410f68aac856e78daeaf3
Abdul Aariz Aman <aarizaman.talim965@gmail.com>
2024-08-01 10:45:54 +0530

    Add files via upload

---
+++ L33--38/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Hello Aariz")
+    })
+
+    document.addEventListener("DOMContentLoaded", function() {
+        alert("How are you?")
+        })
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        alert("Welcome")
+        })
+
+        
